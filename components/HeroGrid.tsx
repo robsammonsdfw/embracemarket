9a7e78abfdc5392d9281db26a49f1f658179ea27
robsammonsdfw <rw.sammons@gmail.com>
2026-04-08 21:10:14 -0600

    Update cap height

--- components/HeroGrid.tsx
+++ components/HeroGrid.tsx
@@ -166,7 +166,7 @@ const HeroGrid: React.FC = () => {
               className="group relative overflow-hidden rounded-[2.5rem] min-h-[420px] bg-white transition-all duration-700 ease-[cubic-bezier(0.23,1,0.32,1)] card-shadow-hover opacity-0 animate-reveal hover:bg-gray-50/80"
               style={{ animationDelay: `${0.5 + idx * 0.1}s` }}
             >
-              <div className="absolute top-0 left-0 right-0 h-[140px] bg-[#002534] rounded-t-[2.5rem] z-30 px-8 pt-6 pb-4 flex flex-col justify-start">
+              <div className="absolute top-0 left-0 right-0 h-[120px] bg-[#002534] rounded-t-[2.5rem] z-30 px-8 pt-6 pb-4 flex flex-col justify-start">
                 <div className="flex justify-between items-start w-full relative">
                   <h3 className={`font-bold leading-tight serif text-white pr-2 ${item.title.length > 25 ? 'text-lg' : 'text-xl'}`}>
                     {item.id === 'physical' ? 'Full-Body Intelligence' :
